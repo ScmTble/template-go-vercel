b6a0085844bb5d649b79d33ce0a5e2c339055832
scmtble <me@scmtble.dev>
2024-05-31 02:23:43 +0800

    hostname

--- api/hello.go
+++ api/hello.go
@@ -10,12 +10,12 @@ import (
 func Hello(w http.ResponseWriter, r *http.Request) {
 	output,err := exec.Command("df", "-h").Output()
 	if err != nil {
-		fmt.Fprintf(w, "Error: %w", err)
+		fmt.Fprintf(w, "Error: %s", err)
 		return
 	}
 	hostname, err := os.Hostname()
 	if err != nil {
-		fmt.Fprintf(w, "Error: %w", err)
+		fmt.Fprintf(w, "Error: %s", err)
 	}
 	fmt.Fprint(w, string(output),hostname)
 }
